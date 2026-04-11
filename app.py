4df6d7265bcc195e547abce5dc0f0cad6ef7610b
Wilson <Chriswasian@icloud.com>
2026-04-10 17:55:00 -0700

    Fix db.create_all() for gunicorn production

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///clarity.db'
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
